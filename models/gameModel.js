d0fc880d6e82135b7f8559e1d46d2a8c0bb26c42
Faizan <120333905+Faizan-Bin-Qaiser@users.noreply.github.com>
2024-05-22 00:33:17 +0500

    hcanges

--- models/gameModel.js
+++ models/gameModel.js
@@ -4,7 +4,6 @@ const gameSchema = new mongoose.Schema({
     name: String,
     description: String,
     image: String,
-    buyLink: String, 
 });
 
 module.exports = mongoose.model('Game', gameSchema);
